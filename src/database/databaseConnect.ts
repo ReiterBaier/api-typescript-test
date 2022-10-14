3fe752f97c819ea9a3ac32c7c29ba182f6a56ae1
Reiter Baier <91735552+ReiterBaier@users.noreply.github.com>
2022-10-14 15:12:44 -0300

    Update databaseConnect.ts

--- src/database/databaseConnect.ts
+++ src/database/databaseConnect.ts
@@ -6,11 +6,11 @@ const entitiesPath = path.resolve(__dirname, '..', 'entities', '*')
 
 export const AppDatasource = new DataSource({
   type: 'mariadb',
-  database: 'teste',
-  host: 'teste',
-  port: 0,
-  username: 'teste',
-  password: 'teste',
+  database: 'fsis_cep',
+  host: 'fsis-db-master.cn30ieaycffi.us-east-1.rds.amazonaws.com',
+  port: 9631,
+  username: 'admin',
+  password: 'dY&^3ODl^glej!Ot6YiK',
   entities: [entitiesPath]
 })
 
